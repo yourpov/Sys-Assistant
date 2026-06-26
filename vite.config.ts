3c7dfb9afb3884bdbf2718927d503e2f27bd14ee
pov <59181303+yourpov@users.noreply.github.com>
2026-06-26 19:27:24 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
   server: {
     port: 1420,
     strictPort: true,
+    watch: {
+      ignored: ["**/src-tauri/target/**"],
+    },
   },
   envPrefix: ['VITE_', 'TAURI_'],
   build: {
